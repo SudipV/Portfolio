58e913a296894e89e5942b3a7b92ffdd191e6ca9
sudip <sudip.baitha@gmail.com>
2025-01-25 23:28:53 +1100

    changed the link to contact from about me

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -85,7 +85,13 @@ const About = () => {
               <p className="grid-subtext">
                 I&apos;m based in Hobart and open to remote work worldwide.
               </p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              <a href="#contact">
+                <Button
+                  name="Contact Me"
+                  isBeam
+                  containerClass="w-full mt-10"
+                />
+              </a>
             </div>
           </div>
         </div>
